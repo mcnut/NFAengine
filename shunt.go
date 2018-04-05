03a1d8d482f0e642a87a2d0473965271ffb16b80
mcnut <G00328962@gmit.ie>
2018-04-05 14:19:05 +0100

    First commit - Project set up. Created shunt.go

---
+++ shunt.go
@@ -0,0 +1,11 @@
+/* Shunting Yard Algorithm - convert expressions from infix notation to postfix notation. */
+package main
+
+import (
+	"fmt"
+)
+
+//	Test program is working.
+func main() {
+	fmt.Println("Shunting Yard Algorithm")
+}
